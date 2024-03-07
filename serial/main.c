9120603702946de1ce512112363cde980e46385b
kukusum <zdrgnde@gmail.com>
2024-03-07 09:01:33 +0900

    goodmorning

--- serial/main.c
+++ serial/main.c
@@ -5,12 +5,12 @@ int main(void)
 {
 	for (int i = 1; i <= 10; ++i) {
 		int number = getSerialNumber();
-		printf("BENZ_EQ300_%010d\n", number);
+		printf("BENZ_EQ300_% 10d\n", number);
 	}
 	
 	for (int i = 1; i <= 5; ++i) {
 		int number = getSerialNumber();
-		printf("BENZ_EQ300_%010d\n", number);
+		printf("BENZ_EQ300_% 10d\n", number);
 	}
 	
 	return 0;
