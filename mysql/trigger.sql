6643c7eaf5703ee08308db16be74c1d828066610
kukusum <zdrgnde@gmail.com>
2024-03-07 16:18:49 +0900

    20240307 1618

--- mysql/trigger.sql
+++ mysql/trigger.sql
@@ -23,3 +23,26 @@ DELIMITER ;
 select * from Book;
 select * from Book_log;
 insert into Book values (14, '스포츠 과학1', '이상미디어', 25000);
+
+
+-- fnc_interest registration end
+USE `madangdb`;
+DROP function IF EXISTS `fnc_interest`;
+
+DELIMITER $$
+USE `madangdb`$$
+CREATE FUNCTION fnc_interest(price integer)
+RETURNS INTEGER
+BEGIN
+	declare myInterest integer;
+    if price >= 10000 then
+		set myInterest = price * 0.1;
+	else
+		set myInterest = price *0.05;
+	end if;
+RETURN myInterest;
+END$$
+
+DELIMITER ;
+-- fnc_interest registration end
+select custid, orderid, saleprice, fnc_interest(saleprice) 'interest' from Orders;
